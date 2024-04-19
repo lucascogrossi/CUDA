0b4c7e81196cec8c7b226fc8c23dd5addb076a2a
lukas <lucasmachado292@gmail.com>
2024-04-18 22:08:30 -0300

    add random values and error handling

--- Programming Massively Parallel Processors/Chapter2/vecAddParallel.cu
+++ Programming Massively Parallel Processors/Chapter2/vecAddParallel.cu
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #define N 1000
 
+inline cudaError_t checkCuda(cudaError_t result)
+{
+	if (result != cudaSuccess)
+	{
+		fprintf(stderr, "CUDA Runtime Error: %s\n", cudaGetErrorString(result));
+		assert(result == cudaSuccess);
+	}
+	return result;
+}
+
 __global__
 void vecAddkernel(float* A, float* B, float* C, int n) {
-    int i = threadIdx.x + blockDim.x * blockIdx.x;
+    unsigned int i = threadIdx.x + blockDim.x * blockIdx.x;
     if (i < n) {
         C[i] = A[i] + B[i];
     }
@@ -16,20 +27,20 @@ void vecAdd(float* A_h, float* B_h, float* C_h, int n) {
     float *A_d, *B_d, *C_d;
     int size = N * sizeof(float);
 
-    cudaMalloc((void**) &A_d, size);
-    cudaMalloc((void**) &B_d, size);
-    cudaMalloc((void**) &C_d, size);
+    checkCuda( cudaMalloc((void**) &A_d, size) );
+    checkCuda( cudaMalloc((void**) &B_d, size) );
+    checkCuda( cudaMalloc((void**) &C_d, size) );
 
-    cudaMemcpy(A_d, A_h, size, cudaMemcpyHostToDevice);
-    cudaMemcpy(B_d, B_h, size, cudaMemcpyHostToDevice);
+    checkCuda( cudaMemcpy(A_d, A_h, size, cudaMemcpyHostToDevice) );
+    checkCuda( cudaMemcpy(B_d, B_h, size, cudaMemcpyHostToDevice) );
 
     vecAddkernel<<<ceil(n / 256.0), 256>>>(A_d, B_d, C_d, n);
 
-    cudaMemcpy(C_h, C_d, size, cudaMemcpyDeviceToHost);
+    checkCuda( cudaMemcpy(C_h, C_d, size, cudaMemcpyDeviceToHost) );
 
-    cudaFree(A_d);
-    cudaFree(B_d);
-    cudaFree(C_d);
+    checkCuda( cudaFree(A_d) );
+    checkCuda( cudaFree(B_d) );
+    checkCuda( cudaFree(C_d) );
 }
 
 int main() {
@@ -39,15 +50,19 @@ int main() {
     v2 = (float*) malloc(sizeof(float) * N);
     v3 = (float*) malloc(sizeof(float) * N);
 
-    for (int i = 0; i < N; i++) {
-        v1[i] = (float) i;
-        v2[i] = (float) i;
+    for (unsigned int i = 0; i < N; ++i) {
+        v1[i] = rand();
+        v2[i] = rand();
         v3[i] = 0;
     }
+    printf("%f\n", v1[252]);
+    printf("%f\n", v2[252]);
+    printf("%f\n", v3[252]);
 
     vecAdd(v1, v2, v3, N);
+    printf("%f\n", v3[252]);
     
-    for (int i = 0; i < N; i++) {
+    for (unsigned int i = 0; i < N; ++i) {
         if (v3[i] != v1[i] + v2[i]) {
             printf("Error\n");
             return 1;
