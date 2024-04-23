54e80a85c8e3ef4ced4425cc96591998effd4da2
lukas <lucasmachado292@gmail.com>
2024-04-23 10:16:30 -0300

    fix colorToGrayscaleConversion.cu

--- Programming Massively Parallel Processors/Chapter3/colorToGrayscaleConversion.cu
+++ Programming Massively Parallel Processors/Chapter3/colorToGrayscaleConversion.cu
@@ -1,15 +1,18 @@
+int CHANNELS = 3; // We assume that CHANNELS is a constant of value 3, 
+//and its definition is outside the kernel function.
+
 // Each input image is encoded as unsigned chars [0, 255]
 // Each pixel is 3 consecutive chars for the 3 channels (RGB)
 
 __global__ void colorToGrayscaleConvertion(unsigned char* Pout, unsigned char* Pin, int width, int height) {
-    int col = BlockIdx.x * Blockdim.x + threadIdx.x;
-    int row = blockIdx.y * Blockdim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
     if (col < width && row < height) {
         // Get 1D offset for the grayscale image
         int grayOffset = row * width + col;
-        // One can think of the RGN image having CHANNEL
+        // One can think of the RGB image having CHANNEL
         // times more columns than the grayscale image
-        int rgbOffset = grayOffset * CHANNEL;
+        int rgbOffset = grayOffset * CHANNELS;
         unsigned char r = Pin[rgbOffset    ]; // Red value
         unsigned char g = Pin[rgbOffset + 1]; // Green value
         unsigned char b = Pin[rgbOffset + 2]; // Blue value
